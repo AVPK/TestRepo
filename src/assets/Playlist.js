465f74dec0b0e2222f32b497a787e92d280e9213
AVPK <awaisahmedshahid@gmail.com>
2024-03-25 12:56:05 +0500

    assets

---
+++ src/assets/Playlist.js
@@ -0,0 +1,23 @@
+import soundQ from "../assets/soundQ.mp3";
+import soundW from "../assets/soundW.mp3";
+import soundE from "../assets/soundE.mp3";
+import soundA from "../assets/soundA.mp3";
+import soundS from "../assets/soundS.mp3";
+import soundD from "../assets/soundD.mp3";
+import soundZ from "../assets/soundZ.mp3";
+import soundX from "../assets/soundX.mp3";
+import soundC from "../assets/soundC.mp3";
+
+const sounds = [
+  new Audio(soundQ),
+  new Audio(soundW),
+  new Audio(soundE),
+  new Audio(soundA),
+  new Audio(soundS),
+  new Audio(soundD),
+  new Audio(soundZ),
+  new Audio(soundX),
+  new Audio(soundC),
+  ];
+
+export default sounds
